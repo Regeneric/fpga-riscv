70eff452771bfb4023bc73d5753c4185c7ab2ff2
Hubert Batkiewicz <hubert@batkiewi.cz>
2024-10-18 21:13:44 +0200

    RISC-V assembly implementation in Verilog

--- riscv_assembly.v
+++ riscv_assembly.v
@@ -6,6 +6,13 @@ localparam  x0 = 0, x1 = 1, x2 = 2, x3 = 3, x4 = 4, x5 = 5, x6 = 6, x7 = 7,
             x16=16, x17=17, x18=18, x19=19, x20=20, x21=21, x22=22, x23=23, 
             x24=24, x25=25, x26=26, x27=27, x28=28, x29=29, x30=30, x31=31;
 
+// ABI register names
+localparam  zero = x0, ra = x1, sp = x2, gp = x3, tp = x4, t0 = x5, t1 = x6, 
+            t2   = x7, fp = x8, s0 = x8, s1 = x9, a0 =x10, a1 =x11, a2 =x12,
+            a3   =x13, a4 =x14, a5 =x15, a6 =x16, a7 =x17, s2 =x18, s3 =x19,
+            s4   =x20, s5 =x21, s6 =x22, s7 =x23, s8 =x24, s9 =x25, s10=x26,
+            s11  =x27, t3 =x28, t4 =x29, t5 =x30, t6 =x31; 
+
 localparam [31:0] NOP_OPCODE = 32'b0000000_00000_00000_000_00000_0110011;   // add x0, x0, x0
 
 // R-Type  ;  Register Type  ;  register-to-register operations  ;  rd, rs1 and rs2  ;  ADD, SUB  etc.
